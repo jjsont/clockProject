3c5d8721dc5a5643ad2e728f10d520b63f5cc779
Benjamin Wu <bwu3112@bths.edu>
2019-03-24 22:54:51 -0400

    a

--- src/Clock.java
+++ src/Clock.java
@@ -1,4 +1,20 @@
 public class Clock {
+    private int seconds;
+    private int minutes;
+    private int hours;
+
+    public Clock(int seconds, int minutes, int hours){
+        this.seconds = seconds;
+        this.minutes = minutes;
+        this.hours = hours;
+    }
+    public String checkTime(){
+        return ("The time is " + this.hours + " : " + this.minutes + " : " + this.seconds);
+    }
+
+    public void alarm(){
+        
+    }
 
 
 }
---
+++ src/clockRunner.java
@@ -0,0 +1,6 @@
+public class clockRunner {
+    public static void main(String[] args){
+        Clock a = new Clock(5,10,12);
+        System.out.println(a.checkTime());
+    }
+}
